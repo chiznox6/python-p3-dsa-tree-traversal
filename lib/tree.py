0b71d2ee70a5e86a0ba6ac3e91140c3a6ae94e8f
James <james.kariuki5@student.moringaschool.com>
2025-08-31 11:05:43 +0300

    initial commit

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,18 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root):
+        self.root = root
 
-  def get_element_by_id(self, id):
+    def get_element_by_id(self, target_id):
+        """Depth-first search to find a node with the given id."""
+        nodes_to_visit = [self.root]
+
+        while nodes_to_visit:
+            node = nodes_to_visit.pop(0)
+            if node['id'] == target_id:
+                return node
+            # DFS: add children to the beginning
+            nodes_to_visit = node['children'] + nodes_to_visit
+
+        return None
+
     pass
